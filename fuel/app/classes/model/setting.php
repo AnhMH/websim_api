b59498fb95eccab94c158176b0516885c2e98079
AnhMH <anhmh@lifull-tech.vn>
2018-08-05 14:25:15 +0700

    Modify settings

--- fuel/app/classes/model/setting.php
+++ fuel/app/classes/model/setting.php
@@ -12,72 +12,75 @@ use Fuel\Core\DB;
  * @copyright Oceanize INC
  */
 class Model_Setting extends Model_Abstract {
-    
+
     /** @var array $_properties field of table */
     protected static $_properties = array();
-
     protected static $_observers = array(
         'Orm\Observer_CreatedAt' => array(
-            'events'          => array('before_insert'),
+            'events' => array('before_insert'),
             'mysql_timestamp' => false,
         ),
         'Orm\Observer_UpdatedAt' => array(
-            'events'          => array('before_update'),
+            'events' => array('before_update'),
             'mysql_timestamp' => false,
         ),
     );
 
     /** @var array $_table_name name of table */
     protected static $_table_name = '';
-    
+
     /**
      * Get general data
      * @param type $param
      * @return boolean
      */
     public static function get_general_data($param) {
+        if (!empty($param['from_admin'])) {
+            return self::get_general_data_for_admin($param);
+        }
+
         // Init
         $data = array();
-        
+
         // Get categories
         $data['cates'] = Model_Cate::get_all(array());
-        
+
         // Get tags
         $data['tags'] = Model_Tag::get_all(array());
-        
+
         // Get sub cate
         $data['sub_cates'] = Model_Sub_Cate::get_all(array());
-        
+
         // Get main menu
         $data['main_menu'] = Model_Page::get_all(array(
-            'is_main_menu' => 1
+                    'is_main_menu' => 1
         ));
-        
+
         // Get footer menu
         $data['footer_menu'] = Model_Page::get_all(array(
-            'is_footer_menu' => 1
+                    'is_footer_menu' => 1
         ));
-        
+
         // Get news
         $data['news'] = Model_New::get_all(array(
-            'page' => 1,
-            'limit' => 10
+                    'page' => 1,
+                    'limit' => 10
         ));
-        
+
         // Get address
         if (!empty($param['get_address'])) {
             $data['provinces'] = Model_Province::get_all(array());
         }
-        
+
         // Get hotlines
         $data['hotlines'] = Model_Hotline::get_all(array());
-        
+
         // Get admin setting
         $data['admin'] = Model_Admin::get_master_info(array());
-        
+
         return $data;
     }
-    
+
     /**
      * Get general data
      * @param type $param
@@ -86,7 +89,7 @@ class Model_Setting extends Model_Abstract {
     public static function get_address_data($param) {
         // Init
         $data = array();
-        
+
         if (!empty($param['type'])) {
             if ($param['type'] == 'district') {
                 $data = Model_District::get_all($param);
@@ -94,7 +97,52 @@ class Model_Setting extends Model_Abstract {
                 $data = Model_Ward::get_all($param);
             }
         }
-        
+
         return $data;
     }
+
+    /**
+     * Get general data
+     * @param type $param
+     * @return boolean
+     */
+    public static function get_general_data_for_admin($param) {
+        // Init
+        $data = array();
+
+        // Get orders
+        $data['orders'] = DB::select(
+                        DB::expr("COUNT(id) as cnt")
+                )
+                ->from('orders')
+                ->execute()
+                ->offsetGet(0);
+
+        // Get products
+        $data['products'] = DB::select(
+                        DB::expr("COUNT(id) as cnt")
+                )
+                ->from('products')
+                ->execute()
+                ->offsetGet(0);
+
+        // Get suppliers
+        $data['suppliers'] = DB::select(
+                        DB::expr("COUNT(id) as cnt")
+                )
+                ->from('suppliers')
+                ->execute()
+                ->offsetGet(0);
+
+        // Get news
+        $data['news'] = DB::select(
+                        DB::expr("COUNT(id) as cnt")
+                )
+                ->from('news')
+                ->execute()
+                ->offsetGet(0);
+
+        return $data;
+    }
+
 }
